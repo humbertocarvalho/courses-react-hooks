57987fc4b7f75ecdd82965b9407ecaa0de214175
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-09-11 06:56:59 -0300

    useEffect Hook.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [techs, setTechs] = useState(['ReacjJS', 'ReactNative']);
+  const [techs, setTechs] = useState([]);
   const [tech, setTech] = useState('');
 
   function handleAdd() {
     setTechs([...techs, tech]);
     setTech('');
   }
+
+  // Array vazio é uma vez só, como se fosse o componentDidMount
+  useEffect(() => {
+    const techsStorage = localStorage.getItem('techs');
+
+    if (techsStorage) {
+      setTechs(JSON.parse(techsStorage));
+    }
+
+    // Como se fosse o componentDidUnmont.
+    return () => {};
+  }, []);
+
+  // Vai disparar sempre que a dependencia que passar no array, sofrer alteração
+  // TIpo componentDidUpdate, testando se teve alteração do estado.
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
 
   return (
     <>
